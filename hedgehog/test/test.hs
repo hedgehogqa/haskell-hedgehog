3563e8c33baaf9da8c2607d70148e9e9b00e938f
Dave Laing <dave.laing.80@gmail.com>
2017-04-08 14:04:43 +1000

    Adds round-trip tests for `Read` and `Show` instances.

--- hedgehog/test/test.hs
+++ hedgehog/test/test.hs
@@ -2,13 +2,17 @@ import           Control.Monad (unless)
 import           System.IO (BufferMode(..), hSetBuffering, stdout, stderr)
 import           System.Exit (exitFailure)
 
+import           Hedgehog
+
+import           Text (textProperties)
+
 main :: IO ()
 main = do
   hSetBuffering stdout LineBuffering
   hSetBuffering stderr LineBuffering
 
-  results <- sequence [
-    ]
+  results <- sequence $
+    fmap check textProperties ++ []
 
   unless (and results) $
     exitFailure
