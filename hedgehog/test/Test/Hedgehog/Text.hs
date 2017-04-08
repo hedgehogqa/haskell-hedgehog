a29b5b39fb31f1d49dfd7e2b7c7f5288c0a93eb9
Jacob Stanley <jacob@stanley.io>
2017-04-08 18:37:22 +1000

    Make read/show tests idiomatic hedgehog

---
+++ hedgehog/test/Test/Hedgehog/Text.hs
@@ -0,0 +1,74 @@
+{-# LANGUAGE TemplateHaskell #-}
+module Test.Hedgehog.Text where
+
+import           Data.Int (Int64)
+import           Data.Typeable (Typeable)
+
+import           Hedgehog
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
+
+
+genSize :: Monad m => Gen m Size
+genSize =
+  Size <$> Gen.enumBounded
+
+genOdd :: Monad m => Gen m Int64
+genOdd =
+  let
+    mkOdd x =
+      if odd x then
+        x
+      else
+        pred x
+  in
+    mkOdd <$> Gen.int64 (Range.constant 1 maxBound)
+
+genSeed :: Monad m => Gen m Seed
+genSeed =
+  Seed <$> Gen.enumBounded <*> genOdd
+
+genPred :: Monad m => Gen m Int
+genPred =
+  Gen.int (Range.constant 0 11)
+
+genString :: Monad m => Gen m String
+genString =
+  Gen.string (Range.constant 0 100) Gen.alpha
+
+checkShowAppend :: (Typeable a, Show a) => Gen IO a -> Property
+checkShowAppend g =
+  property $ do
+    p <- forAll genPred
+    a <- forAll g
+    r <- forAll genString
+    s <- forAll genString
+    showsPrec p a r ++ s  === showsPrec p a (r ++ s)
+
+trippingReadShow :: (Eq a, Typeable a, Show a, Read a) => Gen IO a -> Property
+trippingReadShow g =
+  property $ do
+    p <- forAll genPred
+    a <- forAll g
+    assert $
+      elem (a, "") (readsPrec p (showsPrec p a ""))
+
+prop_show_append_size :: Property
+prop_show_append_size =
+  checkShowAppend genSize
+
+prop_tripping_append_size :: Property
+prop_tripping_append_size =
+  trippingReadShow genSize
+
+prop_show_append_seed :: Property
+prop_show_append_seed =
+  checkShowAppend genSeed
+
+prop_tripping_append_seed :: Property
+prop_tripping_append_seed =
+  trippingReadShow genSeed
+
+tests :: IO Bool
+tests =
+  $$(checkConcurrent)
