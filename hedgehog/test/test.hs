e5631bef3403621ebee8fc039ecd1cb782271ee6
Erik de Castro Lopo <erikd@mega-nerd.com>
2019-03-20 08:40:23 +1100

    Add a testsuite runner function
    And actually use one in the Hedhehog tests.
    
    Closes: https://github.com/hedgehogqa/haskell-hedgehog/issues/97

--- hedgehog/test/test.hs
+++ hedgehog/test/test.hs
@@ -1,20 +1,12 @@
-import           Control.Monad (unless)
-import           System.IO (BufferMode(..), hSetBuffering, stdout, stderr)
-import           System.Exit (exitFailure)
+import           Hedgehog (runTests)
 
 import qualified Test.Hedgehog.Seed
 import qualified Test.Hedgehog.Text
 
 
 main :: IO ()
-main = do
-  hSetBuffering stdout LineBuffering
-  hSetBuffering stderr LineBuffering
-
-  results <- sequence [
+main =
+  runTests [
       Test.Hedgehog.Text.tests
     , Test.Hedgehog.Seed.tests
     ]
-
-  unless (and results) $
-    exitFailure
