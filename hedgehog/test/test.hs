833090f3645672a6c5f37f36a7d9a4c3e0cd23b3
Jacob Stanley <jacob@stanley.io>
2019-04-30 06:44:08 +0800

    Fixed state machine duplicate Vars when shrinking

--- hedgehog/test/test.hs
+++ hedgehog/test/test.hs
@@ -1,5 +1,7 @@
 import           Hedgehog.Main (defaultMain)
 
+import qualified Test.Hedgehog.Applicative
+import qualified Test.Hedgehog.Filter
 import qualified Test.Hedgehog.Seed
 import qualified Test.Hedgehog.Text
 import qualified Test.Hedgehog.Zip
@@ -8,7 +10,9 @@ import qualified Test.Hedgehog.Zip
 main :: IO ()
 main =
   defaultMain [
-      Test.Hedgehog.Text.tests
+      Test.Hedgehog.Applicative.tests
+    , Test.Hedgehog.Filter.tests
     , Test.Hedgehog.Seed.tests
+    , Test.Hedgehog.Text.tests
     , Test.Hedgehog.Zip.tests
     ]
