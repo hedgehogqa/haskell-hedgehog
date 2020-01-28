a713b5a1772622c92908896241e7716365346b7d
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2020-01-28 14:28:35 +0100

    Remove monadApplicative from hedgehog-test-laws
    As per the docs, monad also contains these properties:
    http://hackage.haskell.org/package/checkers-0.5.0/docs/Test-QuickCheck-Classes.html#v:monadApplicative

--- hedgehog-test-laws/test/test.hs
+++ hedgehog-test-laws/test/test.hs
@@ -16,7 +16,7 @@ import           Test.QuickCheck (choose, vector, coarbitraryIntegral, property)
 
 import           Test.QuickCheck (arbitrary1)
 import           Test.QuickCheck.Checkers (EqProp(..), eq)
-import           Test.QuickCheck.Classes (applicative, monad, monadApplicative)
+import           Test.QuickCheck.Classes (applicative, monad)
 import           Test.Tasty (TestTree, defaultMain, testGroup)
 import           Test.Tasty.ExpectedFailure (ignoreTest)
 import           Test.Tasty.QuickCheck (testProperties)
@@ -36,19 +36,16 @@ instances =
         testBatch <$> [
             applicative (undefined :: TreeT Maybe (Bool, Char, Int))
           , monad (undefined :: TreeT Maybe (Bool, Char, Int))
-          , monadApplicative (undefined :: TreeT (Either Bool) (Char, Int))
           ]
     , testGroup "NodeT" $
         testBatch <$> [
             applicative (undefined :: NodeT Maybe (Bool, Char, Int))
           , monad (undefined :: NodeT Maybe (Bool, Char, Int))
-          , monadApplicative (undefined :: NodeT (Either Bool) (Char, Int))
           ]
     , testGroup "GenT" $
         ignoreTest . testBatch <$> [
             applicative (undefined :: GenT Maybe (Bool, Char, Int))
           , monad (undefined :: GenT Maybe (Bool, Char, Int))
-          , monadApplicative (undefined :: GenT (Either Bool) (Char, Int))
           ]
     ]
 
