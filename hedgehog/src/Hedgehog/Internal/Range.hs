9a88f445e3c209aaca20c60c400bb2d99bfbbd09
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2020-10-08 15:45:48 +0200

    Style nits (STYLE_GUIDE.md)

--- hedgehog/src/Hedgehog/Internal/Range.hs
+++ hedgehog/src/Hedgehog/Internal/Range.hs
@@ -307,8 +307,9 @@ scaleLinear sz0 z0 n0 =
 
     -- @rng@ has magnitude 1 bigger than the biggest diff
     -- i.e. it specifies the range the diff can be in [0,rng)
-    -- with the upper bound being exclusive
-    rng = n - z + signum (n - z)
+    -- with the upper bound being exclusive.
+    rng =
+      n - z + signum (n - z)
 
     diff =
       (rng * fromIntegral sz) `quot` 100
