c9066ce8885f7ac69b45ec4023d7969d5ace8a7d
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2020-11-20 15:39:36 +0100

    Update hedgehog/src/Hedgehog/Internal/Property.hs

--- hedgehog/src/Hedgehog/Internal/Property.hs
+++ hedgehog/src/Hedgehog/Internal/Property.hs
@@ -859,7 +859,7 @@ evalMaybe = \case
     pure x
 
 -- | Fails the test if the action throws an exception, or if the
---   the 'Just'.
+--   'Maybe' is 'Nothing', otherwise returns the value in the 'Just'.
 --
 evalMaybeM :: (MonadTest m, Show a, MonadCatch m, HasCallStack) => m (Maybe a) -> m a
 evalMaybeM =
