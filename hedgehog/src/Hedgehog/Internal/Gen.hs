2f46aa9f8162e25b4825cc8096f5346c29208351
Patrick Thomson <patrickt@github.com>
2021-11-01 19:11:58 -0400

    Support GHC 9.2.
    Widens template-haskell bounds and removes `mappend` definitions in
    favor of defaulting to `<>`, as per `-Wnoncanonical-monoid-instances`.

--- hedgehog/src/Hedgehog/Internal/Gen.hs
+++ hedgehog/src/Hedgehog/Internal/Gen.hs
@@ -472,9 +472,6 @@ instance (Monad m, Semigroup a) => Semigroup (GenT m a) where
     liftA2 (Semigroup.<>)
 
 instance (Monad m, Monoid a) => Monoid (GenT m a) where
-  mappend =
-    liftA2 mappend
-
   mempty =
     return mempty
 
